1a14faf85bd2d21c61da587981ad9bdfdfe9f174
Euardo Fernandez <yo@edufdez.es>
2020-12-14 23:30:06 +0100

    zip and unzip

---
+++ lib/utils/ziputils.ts
@@ -0,0 +1,30 @@
+import * as archiver from "archiver";
+import { Error } from "aws-sdk/clients/servicecatalog";
+import { createWriteStream, createReadStream, readdirSync } from "fs";
+import { Extract } from "unzipper";
+
+export function zipDirectory(source: string, out: string): Promise<void> {
+  const archive = archiver("zip", { zlib: { level: 0 } });
+  const stream = createWriteStream(out);
+
+  return new Promise((resolve, reject) => {
+    archive
+      .directory(source, false)
+      .on("error", (err) => reject(err))
+      .pipe(stream);
+
+    stream.on("close", () => resolve());
+    archive.finalize();
+  });
+}
+
+export function unZipDirectory(source: string, out: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    createReadStream(source)
+      .pipe(Extract({ path: out }))
+      .on("finish", (err: Error) => {
+        if (err) return reject(err);
+        else resolve();
+      });
+  });
+}
